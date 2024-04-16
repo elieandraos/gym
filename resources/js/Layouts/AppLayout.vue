a42d29b0fd0ef868a50d63baadd8a677280d1503
Elie Andraos <elieandraos31@gmail.com>
2024-04-16 13:49:03 +0300

    :fire: remove unused layout header slot

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -241,13 +241,6 @@
                 </div>
             </nav>
 
-            <!-- Page Heading -->
-<!--            <header v-if="$slots.header" class="bg-white shadow">-->
-<!--                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">-->
-<!--                    <slot name="header" />-->
-<!--                </div>-->
-<!--            </header>-->
-
             <!-- Page Content -->
             <main>
                 <slot />
