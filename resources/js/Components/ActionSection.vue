4498a11badd70e0688bc9f0f1f20d61abf918e06
Elie Andraos <elieandraos31@gmail.com>
2024-04-19 14:47:35 +0300

    :art: tweak action and form cards style

--- resources/js/Components/ActionSection.vue
+++ resources/js/Components/ActionSection.vue
@@ -9,8 +9,8 @@
             </template>
         </SectionTitle>
 
-        <div class="mt-5 md:mt-0 md:col-span-2">
-            <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+        <div class="mt-5 mx-5 sm:mx-0 md:mt-0 md:col-span-2">
+            <div class="px-4 py-5 sm:p-6 bg-white shadow rounded-lg">
                 <slot name="content" />
             </div>
         </div>
--- resources/js/Components/FormSection.vue
+++ resources/js/Components/FormSection.vue
@@ -9,17 +9,17 @@
             </template>
         </SectionTitle>
 
-        <div class="mt-5 md:mt-0 md:col-span-2">
+        <div class="mt-5 mx-5 sm:mx-0 md:mt-0 md:col-span-2">
             <form @submit.prevent="$emit('submitted')">
                 <div
-                    class="px-4 py-5 bg-white sm:p-6 shadow"
+                    class="px-4 py-5 bg-white sm:p-6 shadow-lg rounded-t-lg"
                     :class="hasActions ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md'">
                     <div class="grid grid-cols-6 gap-6">
                         <slot name="form" />
                     </div>
                 </div>
 
-                <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-end sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                <div v-if="hasActions" class="flex items-center justify-end px-4 py-3 bg-gray-50 text-end sm:px-6 shadow rounded-b-lg">
                     <slot name="actions" />
                 </div>
             </form>
