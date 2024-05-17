40532544e78190b49945cc5669b81a8e48bf9dce
Elie Andraos <elieandraos31@gmail.com>
2024-05-17 10:59:20 +0300

    :art: hook up Inter google font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,10 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
