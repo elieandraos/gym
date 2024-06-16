590eb7a861c01bcb65361df99c10f02117df1fc7
Elie Andraos <elieandraos31@gmail.com>
2024-06-16 17:39:36 +0300

    :zap: mark past booking slots compelete command

---
+++ app/Console/Commands/MarkBookingSlotsComplete.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\Status;
+use App\Models\BookingSlot;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class MarkBookingSlotsComplete extends Command
+{
+    protected $signature = 'lift-station:mark-booking-slots-complete';
+
+    protected $description = 'Updates past booking slots status to complete';
+
+    public function handle() : void
+    {
+        BookingSlot::where('end_time', '<', Carbon::today())
+            ->whereNotIn('status', [ Status::Complete, Status::Cancelled, Status::Frozen] )
+            ->update([
+                'status' => Status::Complete
+            ]);
+    }
+}
---
+++ tests/Feature/CommandMarkBookingSlotsCompleteTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Enums\Status;
+use App\Models\BookingSlot;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Artisan;
+
+it('marks past booking slots as complete', function () {
+    $upcomingSlot = BookingSlot::factory()->create([
+        'end_time' => Carbon::now()->addHour(),
+        'status' => Status::Upcoming
+    ]);
+
+    $cancelledSlot = BookingSlot::factory()->create([
+        'end_time' => Carbon::now()->addHour(),
+        'status' => Status::Cancelled
+    ]);
+
+    // Travel in time to tomorrow and run the command
+    Carbon::setTestNow(Carbon::tomorrow());
+    Artisan::call('lift-station:mark-booking-slots-complete');
+
+    // Assert
+    expect($upcomingSlot->fresh()->status)->toBe(Status::Complete->value);
+    expect($cancelledSlot->fresh()->status)->toBe(Status::Cancelled->value);
+
+    // Reset time
+    Carbon::setTestNow();
+});
