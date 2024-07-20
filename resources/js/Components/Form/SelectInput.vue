d79cabf56f0f053367d4f3ca7e9fcc7509882f53
Elie Andraos <elieandraos31@gmail.com>
2024-07-21 00:18:38 +0300

    :zap: support for dimentional array

--- resources/js/Components/Form/SelectInput.vue
+++ resources/js/Components/Form/SelectInput.vue
@@ -5,11 +5,11 @@
             ref="selectInput"
             v-bind="$attrs"
             class="p-2 text-sm border border-zinc-200 hover:border-zinc-300 rounded-lg focus:outline-none focus:ring-0 bg-none cursor-pointer appearance-none"
-            :class="[size !== 'auto' ? 'min-w-36 lg:min-w-52 w-full' : 'min-w-12', model === '' ? 'text-zinc-400' : '']"
+            :class="[size !== 'auto' ? 'min-w-36 lg:min-w-52 w-full' : 'min-w-12', model === '' || !model ? 'text-zinc-400' : '']"
         >
             <option disabled value="" class="text-zinc-300" v-if="placeholder">{{ placeholder }}</option>
-            <option v-for="option in options" :key="option" :value="option">
-                {{ option }}
+            <option v-for="option in options" :key="optionKey(option)" :value="optionValue(option)">
+                {{ optionName(option) }}
             </option>
         </select>
         <span class="absolute right-2 top-1/2 transform -translate-y-1/2 pointer-events-none">
@@ -23,6 +23,11 @@ import { ref, watch } from 'vue'
 import { ChevronUpDownIcon } from '@heroicons/vue/24/solid/index.js'
 
 const props = defineProps({
+    modelValue: {
+        type: [ String, Number, null],
+        default: '',
+        required: true
+    },
     options: {
         type: Array,
         default: () => [],
@@ -40,13 +45,34 @@ const props = defineProps({
     },
 })
 
-const model = ref('')
+const emit = defineEmits(['update:modelValue'])
+const model = ref(props.modelValue)
+
+watch(model, (newValue) => {
+    emit('update:modelValue', newValue)
+})
+
+watch(() => props.modelValue, (newValue) => {
+    model.value = newValue
+}, { immediate: true })
 
 watch(() => props.options, () => {
     if (!model.value && props.placeholder) {
         model.value = ''
     }
-});
+}, { immediate: true })
+
+const optionKey = (option) => {
+    return typeof option === 'object' ? option.value : option
+}
+
+const optionValue = (option) => {
+    return typeof option === 'object' ? option.value : option
+}
+
+const optionName = (option) => {
+    return typeof option === 'object' ? option.name : option
+}
 </script>
 
 <style scoped>
