cdfaa352c139baae36a6a8eb6527831d682e03f2
Elie Andraos <elieandraos31@gmail.com>
2025-05-22 13:41:09 +0300

    :technologist: improve coding style

--- app/Http/Controllers/Admin/WeeklyCalendarController.php
+++ app/Http/Controllers/Admin/WeeklyCalendarController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CalendarWeekCollection;
-use App\Http\Resources\UserResource;
 use App\Models\Booking;
 use Carbon\Carbon;
 use Carbon\CarbonInterface;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class WeeklyCalendarController extends Controller
 {
-    public function index()
+    public function index() : Response
     {
         $today      = Carbon::today();
         $monday     = $today->copy()->startOfWeek(CarbonInterface::MONDAY);
