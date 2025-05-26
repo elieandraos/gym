481d4b9b5c8c729a7ec1bc15b775ac51f4f62874
Elie Andraos <elieandraos31@gmail.com>
2025-05-26 17:29:48 +0300

    :lipstick: css tweaks

--- resources/js/Components/Form/SwitchInput.vue
+++ resources/js/Components/Form/SwitchInput.vue
@@ -1,21 +1,18 @@
 <template>
-    <div class="px-1 py-0.5 w-13 bg-zinc-100 border border-zinc-200 rounded-full cursor-pointer" @click="model = !model">
-        <div
-            :class="{'translate-x-5 bg-zinc-800': model, 'bg-white': !model}"
-            class="transform transition-transform w-4 h-4 rounded-full">
-        </div>
-    </div>
+    <button
+        type="button"
+        role="switch"
+        :aria-checked="model"
+        @click="model = !model"
+        class="inline-flex items-center h-6 w-10 p-1 bg-zinc-100 border border-zinc-200 rounded-full cursor-pointer focus:outline-none  transition-colors duration-200"
+    >
+    <span
+        :class="model ? 'translate-x-4 bg-zinc-800' : 'translate-x-0 bg-white'"
+        class="inline-block w-4 h-4 rounded-full transform transition-transform duration-200"
+    ></span>
+    </button>
 </template>
 
 <script setup>
 const model = defineModel()
 </script>
-
-<style scoped>
-.transform {
-    transition: transform 0.2s ease-in-out;
-}
-.translate-x-5 {
-    transform: translateX(.5rem); /* Move the switch to the right */
-}
-</style>
