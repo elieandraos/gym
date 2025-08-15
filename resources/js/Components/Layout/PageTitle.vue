a88b2397a5432f689d24609cf7f60a86b0a65787
Elie Andraos <elieandraos31@gmail.com>
2025-08-15 22:30:25 +0300

    Ensure sticky page title covers scrolled content

--- resources/js/Components/Layout/PageTitle.vue
+++ resources/js/Components/Layout/PageTitle.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="['flex justify-between items-center mb-8', sticky && 'sticky top-0 z-10 bg-white']">
+    <div :class="['flex justify-between items-center mb-8', sticky && 'sticky top-0 z-10 bg-white -mt-6 pt-6']">
         <h2 class="text-2xl/8 font-bold text-zinc-950 sm:text-xl/8 grow capitalize">
             <slot></slot>
         </h2>
