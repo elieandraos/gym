bb68301b55f9c2bf27eab8d0421a2cbe3d234d03
Elie Andraos <elieandraos31@gmail.com>
2025-09-29 22:15:11 +0300

    ``` style: add top margin to calendar day headers for improved spacing ```

--- resources/js/Components/Layout/Calendar/components/CalendarHeader.vue
+++ resources/js/Components/Layout/Calendar/components/CalendarHeader.vue
@@ -24,7 +24,7 @@
         </div>
 
         <!-- Days header row -->
-        <div class="flex-none bg-white shadow ring-1 ring-black/5 sm:pr-8">
+        <div class="flex-none mt-1 bg-white shadow ring-1 ring-black/5 sm:pr-8">
             <div class="-mr-px hidden grid-cols-6 divide-x divide-gray-100 border-r border-gray-100 text-sm text-gray-500 sm:grid">
                 <div class="col-end-1 w-14"></div>
                 <template v-for="day in headerDays" :key="day.day">
