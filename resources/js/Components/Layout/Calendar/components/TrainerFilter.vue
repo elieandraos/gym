d1a3204a86767df27c2520c6d51a062a3a73419a
Elie Andraos <elieandraos31@gmail.com>
2025-10-01 14:45:05 +0300

    ``` refactor: inline color mapping and improve label styling in trainer filter ```

--- resources/js/Components/Layout/Calendar/components/TrainerFilter.vue
+++ resources/js/Components/Layout/Calendar/components/TrainerFilter.vue
@@ -9,17 +9,13 @@
                     @change="updateSelection(id, $event)"
                     class="h-4 w-4 rounded border-zinc-200 accent-black focus:ring-black cursor-pointer"
                 />
-                <span class="ml-2 cursor-pointer" :class="getTextColorClass(color)">{{ first_name }}</span>
+                <span class="ml-1 font-[500] cursor-pointer" :class="getTextColor(color)">{{ first_name }}</span>
             </label>
         </template>
     </div>
 </template>
 
 <script setup>
-import { useColorScheme } from '../composables/useColorScheme.js'
-
-const { getColorScheme } = useColorScheme()
-
 const props = defineProps({
     availableTrainers: {
         type: Array,
@@ -33,8 +29,20 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue', 'filterChange'])
 
-const getTextColorClass = (bgColor) => {
-    return getColorScheme(bgColor).text
+const getTextColor = (bgColor) => {
+    const colorMap = {
+        'bg-blue-50': 'text-blue-300',
+        'bg-amber-100': 'text-amber-300',
+        'bg-pink-50': 'text-pink-300',
+        'bg-green-50': 'text-green-300',
+        'bg-gray-100': 'text-gray-300',
+        'bg-purple-100': 'text-purple-300',
+        'bg-cyan-100': 'text-cyan-300',
+        'bg-yellow-100': 'text-yellow-300',
+        'bg-lime-100': 'text-lime-300',
+        'bg-red-100': 'text-red-300',
+    }
+    return colorMap[bgColor] || 'text-gray-100'
 }
 
 const updateSelection = (trainerId, event) => {
