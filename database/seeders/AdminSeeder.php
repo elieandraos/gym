e640aabe6828d2d5d25d18d647bcef56a9d72980
Elie Andraos <elieandraos31@gmail.com>
2025-11-05 09:02:38 +0200

    :seedling: remove faker from production seeders

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Enums\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class AdminSeeder extends Seeder
         User::query()->create([
             'name' => 'Owner',
             'email' => 'owner@lift-station.fitness',
+            'password' => Hash::make('password'),
             'role' => Role::Admin,
             'settings' => User::getDefaultSettings(Role::Admin->value),
         ]);
