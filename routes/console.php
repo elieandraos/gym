c8f99a3cfc498a05c51f7d2c226fb220a521ba4e
Elie Andraos <elieandraos31@gmail.com>
2025-12-05 09:56:03 +0200

    :wrench: schedule timezone

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,12 @@
 
 use App\Console\Commands\MarkBookingSlotsComplete;
 use App\Console\Commands\SendBookingSlotReminders;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command(MarkBookingSlotsComplete::class)
+    ->everyThirtyMinutes()
+    ->timezone(env('APP_TIMEZONE', 'Asia/Beirut'));
 
-Schedule::command(MarkBookingSlotsComplete::class)->hourly();
-Schedule::command(SendBookingSlotReminders::class)->dailyAt('21:00');
+Schedule::command(SendBookingSlotReminders::class)
+    ->dailyAt('21:00')
+    ->timezone(env('APP_TIMEZONE', 'Asia/Beirut'));
